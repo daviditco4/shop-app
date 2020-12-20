913db362b277c67c083515e1dcd87b3675aa2e5c
David Itcovici <daviditco4@gmail.com>
2020-12-19 22:33:33 -0300

    Fix dismissible widget again

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -14,7 +14,7 @@ class CartItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final cartItem = Provider.of<ci.CartItem>(context, listen: false);
 
-    return Card(
+    return Container(
       margin: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 4.0),
       child: Dismissible(
         key: ValueKey(productId),
@@ -28,25 +28,27 @@ class CartItem extends StatelessWidget {
           alignment: Alignment.centerLeft,
           child: const Icon(Icons.delete, size: 30.0, color: Colors.white),
         ),
-        child: ListTile(
-          leading: Consumer<Products>(
-            builder: (_, products, __) {
-              const imageSize = 40.0;
-              return Image.network(
-                products.findById(productId).imageUrl,
-                width: imageSize,
-                height: imageSize,
-                fit: BoxFit.fill,
-              );
-            },
-          ),
-          title: Text(cartItem.title),
-          subtitle: Text('${cartItem.quantity} x \$${cartItem.price}'),
-          trailing: Chip(
-            backgroundColor: Theme.of(context).primaryColorDark,
-            label: Text(
-              '\$${cartItem.totalPrice}',
-              style: const TextStyle(color: Colors.white),
+        child: Card(
+          child: ListTile(
+            leading: Consumer<Products>(
+              builder: (_, products, __) {
+                const imageSize = 40.0;
+                return Image.network(
+                  products.findById(productId).imageUrl,
+                  width: imageSize,
+                  height: imageSize,
+                  fit: BoxFit.fill,
+                );
+              },
+            ),
+            title: Text(cartItem.title),
+            subtitle: Text('${cartItem.quantity} x \$${cartItem.price}'),
+            trailing: Chip(
+              backgroundColor: Theme.of(context).primaryColorDark,
+              label: Text(
+                '\$${cartItem.totalPrice}',
+                style: const TextStyle(color: Colors.white),
+              ),
             ),
           ),
         ),

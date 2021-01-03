70cb8542a9e36d9ee3fec5ccf79fd90529ecef64
David Itcovici <daviditco4@gmail.com>
2021-01-03 19:29:30 -0300

    Save form input in a new product

--- lib/pages/your_products/edit_product_page.dart
+++ lib/pages/your_products/edit_product_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import '../../models/products/product.dart';
+import '../../models/utils/price.dart';
+
 class EditProductPage extends StatefulWidget {
   static const routeName = '/edit-product';
 
@@ -9,10 +12,36 @@ class EditProductPage extends StatefulWidget {
 }
 
 class _EditProductPageState extends State<EditProductPage> {
+  final _formInput = {
+    'id': null,
+    'title': '',
+    'description': '',
+    'price': Price(0.0),
+    'imageUrl': '',
+  };
+  final _formKey = GlobalKey<FormState>();
   final _priceFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
   final _imageUrlFocusNode = FocusNode();
   final _imageUrlController = TextEditingController();
+
+  void _saveForm() {
+    _formKey.currentState.save();
+
+    final newProduct = Product(
+      id: _formInput['id'],
+      title: _formInput['title'],
+      description: _formInput['description'],
+      price: _formInput['price'],
+      imageUrl: _formInput['imageUrl'],
+    );
+
+    print(newProduct.id);
+    print(newProduct.title);
+    print(newProduct.description);
+    print(newProduct.price);
+    print(newProduct.imageUrl);
+  }
 
   void _updateImage() {
     if (!_imageUrlFocusNode.hasFocus) setState(() {});
@@ -40,15 +69,20 @@ class _EditProductPageState extends State<EditProductPage> {
     const imageSize = 75.0;
 
     return Scaffold(
-      appBar: AppBar(title: const Text('Edit Product')),
+      appBar: AppBar(
+        title: const Text('Edit Product'),
+        actions: [FlatButton(onPressed: _saveForm, child: const Text('SAVE'))],
+      ),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Form(
+          key: _formKey,
           child: SingleChildScrollView(
             child: Column(
               children: [
                 TextFormField(
                   decoration: const InputDecoration(labelText: 'Title'),
+                  onSaved: (newVal) => _formInput['title'] = newVal,
                   onFieldSubmitted: (_) {
                     focusScope.requestFocus(_priceFocusNode);
                   },
@@ -58,6 +92,7 @@ class _EditProductPageState extends State<EditProductPage> {
                   focusNode: _priceFocusNode,
                   decoration: const InputDecoration(labelText: 'Price'),
                   keyboardType: TextInputType.number,
+                  onSaved: (v) => _formInput['price'] = Price(double.parse(v)),
                   onFieldSubmitted: (_) {
                     focusScope.requestFocus(_descriptionFocusNode);
                   },
@@ -68,6 +103,7 @@ class _EditProductPageState extends State<EditProductPage> {
                   decoration: const InputDecoration(labelText: 'Description'),
                   maxLines: 3,
                   keyboardType: TextInputType.multiline,
+                  onSaved: (newVal) => _formInput['description'] = newVal,
                 ),
                 Row(
                   crossAxisAlignment: CrossAxisAlignment.end,
@@ -99,6 +135,8 @@ class _EditProductPageState extends State<EditProductPage> {
                           labelText: 'Image URL',
                         ),
                         keyboardType: TextInputType.url,
+                        onSaved: (newVal) => _formInput['imageUrl'] = newVal,
+                        onFieldSubmitted: (_) => _saveForm(),
                       ),
                     ),
                   ],
